92f0ac13a618223371fab813ca820523690ccb7a
Ticki84 <Ticki84@gmail.com>
2016-06-13 18:31:36 +0200

    Initial Version meta file

---
+++ JVCSticker++.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        JVCSticker++
+// @namespace   JVCSticker++
+// @include     http://www.jeuxvideo.com/forums/*
+// @include     https://www.jeuxvideo.com/forums/*
+// @version     1.3.0
+// @grant       GM_addStyle
+// @updateURL   http://jscore.comli.com/JVCSticker++.meta.js
+// @downloadURL http://jscore.comli.com/JVCSticker++.user.js
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @icon        http://jv.stkr.fr/p/1kki
+// @author      Ticki84
+// @copyright   2016+, Ticki84
+// ==/UserScript==
