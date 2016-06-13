b18674c19379e4bd733c665b068cbdbe43f7be03
Ticki84 <Ticki84@gmail.com>
2016-06-13 18:37:37 +0200

    Initial Version Test

--- JVCSticker++.meta.js
+++ JVCSticker++.meta.js
@@ -5,8 +5,8 @@
 // @include     https://www.jeuxvideo.com/forums/*
 // @version     1.3.0
 // @grant       GM_addStyle
-// @updateURL   http://jscore.comli.com/JVCSticker++.meta.js
-// @downloadURL http://jscore.comli.com/JVCSticker++.user.js
+// @updateURL   https://ticki84.github.io/JVCSticker++.meta.js
+// @downloadURL https://ticki84.github.io/JVCSticker++.user.js
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @icon        http://jv.stkr.fr/p/1kki
 // @author      Ticki84
