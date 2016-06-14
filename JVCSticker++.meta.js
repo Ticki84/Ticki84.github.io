20ed3450dd58f552578e93e1d8fe4fa280ef8b20
Ticki84 <Ticki84@gmail.com>
2016-06-14 13:31:37 +0200

    Fixed various bug, added stickers, added Youtube player

--- JVCSticker++.meta.js
+++ JVCSticker++.meta.js
@@ -3,7 +3,7 @@
 // @namespace   JVCSticker++
 // @include     http://www.jeuxvideo.com/forums/*
 // @include     https://www.jeuxvideo.com/forums/*
-// @version     1.3.4
+// @version     1.4.0
 // @grant       GM_addStyle
 // @updateURL   https://ticki84.github.io/JVCSticker++.meta.js
 // @downloadURL https://ticki84.github.io/JVCSticker++.user.js
