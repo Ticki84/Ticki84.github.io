158744c6dcd286f4674c9dfaf7da771432fd5206
Ticki84 <Ticki84@gmail.com>
2016-06-19 22:28:18 +0200

    New version

--- JVCSticker++.meta.js
+++ JVCSticker++.meta.js
@@ -3,12 +3,19 @@
 // @namespace   JVCSticker++
 // @include     http://www.jeuxvideo.com/forums/*
 // @include     https://www.jeuxvideo.com/forums/*
-// @version     1.4.3
 // @grant       GM_addStyle
 // @updateURL   https://ticki84.github.io/JVCSticker++.meta.js
 // @downloadURL https://ticki84.github.io/JVCSticker++.user.js
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     https://ticki84.github.io/jquery.modal.min.js
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getValue
+// @grant       GM_setValue
+// grant       GM_listValues
+// grant       GM_deleteValue
+// grant       GM_log
 // @icon        http://jv.stkr.fr/p/1kki
 // @author      Ticki84
 // @copyright   2016+, Ticki84
+// @noframes
 // ==/UserScript==
