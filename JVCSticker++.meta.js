878f17d61b17b4e28a6407c7b85668559fa7c3cc
Ticki84 <Ticki84@gmail.com>
2016-07-26 16:35:05 +0200

    New Version

--- JVCSticker++.meta.js
+++ JVCSticker++.meta.js
@@ -3,7 +3,7 @@
 // @namespace   JVCSticker++
 // @include     http://www.jeuxvideo.com/forums/*
 // @include     https://www.jeuxvideo.com/forums/*
-// @version     1.5.21
+// @version     1.5.22
 // @grant       GM_addStyle
 // @updateURL   https://ticki84.github.io/JVCSticker++.meta.js
 // @downloadURL https://ticki84.github.io/JVCSticker++.user.js
