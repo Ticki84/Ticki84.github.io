f9e6c6144a78e2fd54676e3253896ac657e558d1
Ticki84 <Ticki84@gmail.com>
2017-05-29 01:02:39 +0200

    New Version

--- JVCSticker++.meta.js
+++ JVCSticker++.meta.js
@@ -3,21 +3,25 @@
 // @namespace   JVCSticker++
 // @include     http://www.jeuxvideo.com/forums/*
 // @include     https://www.jeuxvideo.com/forums/*
-// @version     1.6.2
+// @version     2.0.0
 // @grant       GM_addStyle
 // @updateURL   https://ticki84.github.io/JVCSticker++.meta.js
 // @downloadURL https://ticki84.github.io/JVCSticker++.user.js
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js
 // @require     https://ticki84.github.io/jquery.modal.min.js
+// @require     https://swisnl.github.io/jQuery-contextMenu/dist/jquery.contextMenu.js
+// @require     https://swisnl.github.io/jQuery-contextMenu/dist/jquery.ui.position.min.js
 // @connect     github.io
-// @connect     jvcsticker.site88.net
+// @connect     jvcsticker.esy.es
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_listValues
 // @grant       GM_deleteValue
+// @grant       GM_registerMenuCommand
+// @grant       GM_log
 // @icon        http://jv.stkr.fr/p/1kki
 // @author      Ticki84
-// @copyright   2016+, Ticki84
+// @copyright   2017+, Ticki84
 // @noframes
 // ==/UserScript==
